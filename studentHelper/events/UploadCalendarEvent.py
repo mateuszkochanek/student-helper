41ab06917d96759796ea6c55206fdde209523502
kaszycaO <o_kaszyca@interia.pl>
2020-11-16 13:45:22 +0100

    Upload working

--- studentHelper/events/UploadCalendarEvent.py
+++ studentHelper/events/UploadCalendarEvent.py
@@ -22,24 +22,37 @@ class UploadCalendarEvent(Event):
          try:
              events = Events.objects.get_record_by_client_id(self.get_user().id)
              #TODO repair this stupid
+             # perfectly writed
+             # mega code 9
              for event in events:
                  checked = False
                  extra = Description.objects.get_descriptions(event).first()
                  key = str(event.start_date.weekday())
-                 print(key)
-                 for dict in data:
-                     if key in dict:
-                         dict[key] += '\n' + (str(event.start_date) + ' ' + str(extra.description))
-                         checked = True
-                         break
-                 if checked == False:
-                     data[event.start_date.weekday()] = {
-                            key : str(event.start_date.time) + ' ' + str(extra.description)
-                             # 'end date': event.end_date,
-                             # 'period type': event.period_type,
-                             # 'course': extra.course,
-                             # 'description': extra.description
-                             }
+                 if event.period_type == "ONCE":
+                     for dict in data:
+                         if key in dict:
+                             dict[key] += '\n' + '\n' + (str(event.start_date.time()) + '-' + str(event.end_date.time())
+                                                    + ' ' + str(extra.description))
+                             checked = True
+                             break
+
+                     if checked == False:
+                         data[event.start_date.weekday()] = {
+                                key : str(event.start_date.time()) + '-' + str(event.end_date.time())
+                                         + ' ' + str(extra.description)
+                                 # 'end date': event.end_date,
+                                 # 'period type': event.period_type,
+                                 # 'course': extra.course,
+                                 # 'description': extra.description
+                                 }
+                 elif event.period_type == "DAILY" :
+                     for i in range(7):
+                        if str(i) in data[i]:
+                            data[i][str(i)] += '\n' + '\n' + (str(event.start_date.time()) + ' ' + str(extra.description))
+                        else:
+                            data[i] = { str(i): '\n' + '\n' + (str(event.start_date.time()) + ' ' + str(extra.description)) }
+
+
 
 
          except(EmptyResultSet, MultipleObjectsReturned, ObjectDoesNotExist) as e:
--- studentHelper/views.py
+++ studentHelper/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Post, Events
+from .models import Events
 from .events.UploadCalendarEvent import UploadCalendarEvent
 
 # Create your views here.
