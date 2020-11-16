769372c4c8447602d4b192f73bb32d84e9f96f7f
Domka <dszydlo28@gmail.com>
2020-11-16 12:49:28 +0100

    + default none in course.final

--- studentHelper/models.py
+++ studentHelper/models.py
@@ -51,7 +51,7 @@ class Course(models.Model):
     ECTS = models.IntegerField()
     name = models.CharField(max_length=30)
     type = models.CharField(max_length=1, choices=TYPES)
-    final = models.IntegerField(blank=True)
+    final = models.IntegerField(blank=True, default=None)
     objects = CourseManager()
 
 
