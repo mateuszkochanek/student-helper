a3124ca93983a64d5f6ef0708f9b821d38a44476
mateuszkochanek <mateuszkochanek01@gmail.com>
2020-11-22 17:56:23 +0100

    Quick fix for today's courses

--- studentHelper/views.py
+++ studentHelper/views.py
@@ -26,7 +26,7 @@ def main_view(request):
     events_today = Events.objects.get_events(
         request.user.id,
         timezone.now(),
-        timezone.now().replace(hour=00, minute=00, second=1) + timedelta(days=1)
+        timezone.now().replace(hour=23, minute=59, second=59)
     ).filter(description__course=1).order_by('start_date')[:5]
 
     next_events = Events.objects.filter(client_id=request.user.id, description__course=0, start_date__gte=timezone.now()).order_by('start_date')[:5]
