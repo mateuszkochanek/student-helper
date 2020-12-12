9ccdc3819e09e217810c2086344e5ed053dcba12
kaszycaO <o_kaszyca@interia.pl>
2020-12-12 21:08:04 +0100

    Bug fix - add mark

--- course/views.py
+++ course/views.py
@@ -15,7 +15,7 @@ def course_view(request, pk):
         'marks': Marks.objects.getMarks(pk),
         'goals': Goals.objects.get_records_by_course_id(pk)
     }
-
+    print(context['marks'])
     if context['course'].client_id != request.user:
         return main_view(request)
 
@@ -44,16 +44,16 @@ def add_mark_view(request, pk):
 
 @login_required(login_url='/login')
 def edit_mark_view(request, pk):
-    mark = Marks.objects.get_record_by_id(pk)
+    my_mark = Marks.objects.get_record_by_id(pk)
 
     if request.method == 'POST':
-        mark = MarkForm(request.POST, instance=mark)
+        mark = MarkForm(request.POST, instance=my_mark)
 
         if mark.is_valid():
             mark.save()
-            return redirect('/course/'+str(pk))
+            return redirect('/course/'+str(my_mark.course_id.id))
     else:
         #TODO info?
-        mark = MarkForm(instance=mark)
+        mark = MarkForm(instance=my_mark)
 
     return render(request, "new_mark.html", {"mark_form": mark, "pk": pk})
