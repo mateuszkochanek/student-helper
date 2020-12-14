5963c6f17f44e592d0462a35574a605202ffb7c3
Paweł Data <123pawel99@gmail.com>
2020-12-14 22:54:23 +0100

    Show all courses in avgGrade page

--- avgGrade/views.py
+++ avgGrade/views.py
@@ -7,7 +7,7 @@ from .functions import Functions
 @login_required(login_url='/login/')
 def avg_grade_view(request):
     context = {
-        'all_courses': Functions().get_courses_and_group_courses(request.user.id)
+        'all_courses': Course.objects.get_records_by_client_id(request.user.id)
     }
     return render(request, "avg_grade.html", context)
 
